2d01d1b79ce0d29caf3d7ceb1da4e366283a2a68
Roger <rxgranda@gmail.com>
2015-11-24 06:12:29 -0500

    Correir reconexion

--- server/serverConnection.js
+++ server/serverConnection.js
@@ -8,18 +8,19 @@ function RemoteServer(host, port, namespace){
     this.host=host;
     this.port=port;
     this.namespace=namespace;
+    RemoteServer.reference=this;
 }
 
 
 
 RemoteServer.prototype.startConnection=function (){
-    var reference=this;
     this.client= new WebSocketClient();
     
      
     this.client.on('connectFailed', function(error) {
+
         console.log('Connect Error: ' + error.toString());
-        setTimeout(reference.startConnection, reference.reconnectInterval);
+        setTimeout(RemoteServer.reference.startConnection, RemoteServer.reference.reconnectInterval);
     });
      
     this.client.on('connect', function(connection) {
@@ -27,12 +28,12 @@ RemoteServer.prototype.startConnection=function (){
         
         connection.on('error', function(error) {
             console.log("Connection Error: " + error.toString());
-            setTimeout(reference.startConnection, reference.reconnectInterval);
+            setTimeout(RemoteServer.reference.startConnection, RemoteServer.reference.reconnectInterval);
         });
         
         connection.on('close', function() {
             console.log('echo-protocol Connection Closed');
-            setTimeout(reference.startConnection, reference.reconnectInterval);
+            setTimeout(RemoteServer.reference.startConnection, RemoteServer.reference.reconnectInterval);
         });
         
         connection.on('message', function(message) {
@@ -42,19 +43,22 @@ RemoteServer.prototype.startConnection=function (){
        
     });
         
-     reference.connection=connection;
-     console.log('Hell0');
+     RemoteServer.reference.connection=connection;
+     //console.log('Helo');
   });
-   
-  this.client.connect('ws://'+this.host+':'+this.port+'/'+this.namespace);
+  console.log('Conectando a : ws://'+RemoteServer.reference.host+':'+RemoteServer.reference.port+'/'+RemoteServer.reference.namespace);
+  this.client.connect('ws://'+RemoteServer.reference.host+':'+RemoteServer.reference.port+'/'+RemoteServer.reference.namespace);
  
 };
 
-RemoteServer.prototype.query=function (matricula){
+RemoteServer.prototype.query=function (data){
   if (this.connection.connected) {    
-      this.connection.sendUTF({datos:'1'});
-  }
+      this.connection.sendUTF(data);
+  }else
+    console.log('websocket not connected')
 };
+
+
 
 
 
@@ -62,7 +66,7 @@ server=new RemoteServer('localhost',9000,'test');
 server.startConnection();
 
 
-var request=[{"reuqestId": "5645f7f7ef0bde57344c84de"}];
+request=[{"reuqestId": "5645f7f7ef0bde57344c84de"}];
 request=JSON.stringify(request);
   /*  "student": [
       {
